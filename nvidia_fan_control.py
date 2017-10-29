4f9ae1c72e9c13753b36c3f09b70003273b8761a
liraop <lirapdo@gmail.com>
2017-10-29 00:59:41 -0300

    started python version; temperature added

---
+++ nvidia_fan_control.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import logging, time, subprocess, shlex
+
+
+def main(): 
+	logging.basicConfig(level="INFO")
+
+	
+	logging.info("GPU temperature: %s°C", getGPUTemperature())
+
+
+def getGPUTemperature():
+	textCommand = "nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits"
+	command = shlex.split(textCommand)
+	temperature = subprocess.Popen(command, stdout=subprocess.PIPE, universal_newlines=True)
+
+	return temperature.stdout.read().strip()
+
+main()
