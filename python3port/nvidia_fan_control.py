e750703db632410155ff124f7c77b17960894ea9
liraop <lirapdo@gmail.com>
2017-10-30 23:53:35 -0300

    commandParser function

--- python3port/nvidia_fan_control.py
+++ python3port/nvidia_fan_control.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 
-import logging, time, subprocess, shlex
+import GPU, logging, time, subprocess, shlex
 
+def commandParser(osCommand):
+	"""
+	Simple OS command parser.
 
-def getGPUTemperature():
-	textCommand = "nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits"
-	command = shlex.split(textCommand)
-	temperature = subprocess.Popen(command, stdout=subprocess.PIPE, universal_newlines=True)
+	Args:
+		osCommand: The command to be parsed.
+		e.g.: 'nvidia-smi -L'
 
-	return temperature.stdout.read().strip()
+	Returns:
+		A string array corresponding to the osCommand stdout.
+		e.g.: ['GPU', '0:', 'GeForce', 'GTX', '960', '(UUID:', 'GPU-1d11ae72-cd45-276f-0a0d-b2be8b74e4da)']
 
+	"""
+	command = shlex.split(osCommand)
+	result = subprocess.Popen(command, stdout=subprocess.PIPE, universal_newlines=True)
+
+	return result.stdout.read().split();
 
 def main():
-	logging.basicConfig(level="INFO")
-
-	logging.info("GPU temperature: %s°C", getGPUTemperature())
-
+	print(commandParser("nvidia-smi -L"))
 
 main()
