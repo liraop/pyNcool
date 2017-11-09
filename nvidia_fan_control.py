ba840ac78e8a30c45c04edbef5495c9505e3f068
liraop <lirapdo@gmail.com>
2017-11-08 23:24:07 -0300

    removed method to utils

--- nvidia_fan_control.py
+++ nvidia_fan_control.py
@@ -1,26 +1,8 @@
 #!/usr/bin/env python3
 
-import GPU, logging, time, subprocess, shlex
-
-def commandParser(osCommand):
-	"""
-	Simple OS command parser.
-
-	Args:
-		osCommand: The command to be parsed.
-		e.g.: 'nvidia-smi -L'
-
-	Returns:
-		A string array corresponding to the osCommand stdout.
-		e.g.: ['GPU', '0:', 'GeForce', 'GTX', '960', '(UUID:', 'GPU-1d11ae72-cd45-276f-0a0d-b2be8b74e4da)']
-
-	"""
-	command = shlex.split(osCommand)
-	result = subprocess.Popen(command, stdout=subprocess.PIPE, universal_newlines=True)
-
-	return result.stdout.read().split();
+import GPU, logging, time, subprocess, utils
 
 def main():
-	print(commandParser("nvidia-smi -L"))
+	print(utils.commandParser("nvidia-smi -L"))
 
 main()
