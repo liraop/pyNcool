5d8da1138836edd100589bcc4e7525b96e56e536
liraop <lirapdo@gmail.com>
2017-11-18 22:32:48 -0300

    fix script

--- deprecated/nvidia_fan_control-v1.sh
+++ deprecated/nvidia_fan_control-v1.sh
@@ -8,7 +8,7 @@
 # Script meant to start on user login
 #
 # Requirements:
-# Nvidia GPU, X and 'coolbits 4' option enabled on xorg.conf
+# Nvidia GPU and 'coolbits 4' option enabled on xorg.conf
 #
 # Written by liraop - pedrolira@live.com
 #
@@ -20,25 +20,24 @@ set -o pipefail
 set -o nounset
 
 ## time to rerun temperature check
-checkTime=3
+checkTime=10
 
-out=$(nvidia-settings -a [gpu:0]/GPUFanControlState=1)
 while true; do
    ## get GPU core temperature
    gputemp=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits)
-
+   ## enable fan control
+   nsReturn=$(nvidia-settings -a [gpu:0]/GPUFanControlState=1)
    ## get current fan speed
    startSpeed=$(nvidia-smi --query-gpu=fan.speed --format=csv,noheader,nounits)
 
    printf %b "GPU Temperature: ${gputemp}c\n"
 
    ## check temperature, set target speed, set fan speed
-
    case $gputemp in
       [0-4][0-9])
       targetSpeed=40
       nsReturn=$(nvidia-settings -a [fan:0]/GPUTargetFanSpeed=${targetSpeed})
-      ;;x9spit
+      ;;
       5[0-9])
       targetSpeed=50
       nsReturn=$(nvidia-settings -a [fan:0]/GPUTargetFanSpeed=${targetSpeed})
@@ -56,12 +55,12 @@ while true; do
       ;;
    esac
 
+   ## where the magic happens to fix fan speed status
+   nsReturn=$(nvidia-settings -a [gpu:0]/GPUFanControlState=1)
    ## gets fan final speed
    finalSpeed=$(nvidia-smi --query-gpu=fan.speed --format=csv,noheader,nounits)
 
    msg="Fan speed was ${startSpeed}%\nFan speed is ${finalSpeed}%\n"
-
    printf %b "${msg}"
    sleep ${checkTime}
 done
-out=$(nvidia-settings -a [gpu:0]/GPUFanControlState=0)
