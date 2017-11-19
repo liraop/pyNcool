e581fb480450fcaff0bc07150a40ea27be2c8e05
liraop <lirapdo@gmail.com>
2017-11-19 00:53:26 -0300

    fix linter warnings

--- GPU.py
+++ GPU.py
@@ -1,12 +1,13 @@
 class GPU():
 
-	"""
+    """
 	A graphic processing unit
 
 	"""
 
-	def __init__(self, slot, model):
-		"""
+    def __init__(self, slot, model):
+
+        """
 		Defines the GPU slot and card model.
 
 		GPU slot is the logical slot of the GPU in the system. If it's a single
@@ -15,25 +16,26 @@ class GPU():
 		Card model is stored in `self.model`
 
 		"""
-		self.slot = slot
-		self.model = model
+
+        self.slot = slot
+        self.model = model
 
 
-	def updateInfo(self, temperature, fanSpeed):
-		""" Updates GPU's information
+    def update_info(self, temperature, fanspeed):
+        """ Updates GPU's information
 
 		Current GPU fan speed is stored at `self.fanSpeed`
 
 		Current GPU temperature is stored at `self.temperature`
 
 		"""
-		self.fanSpeed = fanSpeed
-		self.temperature = temperature
+        self.fanspeed = fanspeed
+        self.temperature = temperature
 
-	def __str__(self):
-		""" String representation of the class
+    def __str__(self):
+        """ String representation of the class
 		"""
 
-		summary = "Slot: " + self.slot + "Model: " + self.model
+        summary = "Slot: " + self.slot + "Model: " + self.model
 
-		return summary
+        return summary
