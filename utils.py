d2826b16f4e33db065c9389e949746f4af708588
liraop <lirapdo@gmail.com>
2017-11-21 22:43:30 -0300

    refactor command_parser and get_gpu_count

--- utils.py
+++ utils.py
@@ -10,22 +10,23 @@ def command_parser(oscommand):
     	e.g.: 'nvidia-smi -L'
 
     Returns:
-    	A string array corresponding to the osCommand stdout.
-    	e.g.: ['GPU', '0:', 'GeForce', 'GTX', '960', '(UUID:', 'GPU-1d11ae72-cd45-276f-0a0d-b2be8b74e4da)']
+    	A string corresponding to the osCommand stdout.
+    	e.g.: GPU 0: GeForce GTX 960 (UUID: GPU-1d11ae72-cd45-276f-0a0d-b2be8b74e4da)
+
 
     """
     command = shlex.split(oscommand)
     result = subprocess.Popen(command, stdout=subprocess.PIPE, universal_newlines=True)
 
-    return result.stdout.read().split()
+    return result.stdout.read()
 
 def get_gpu_count():
     """
-    Get local machine total attached formatGPUlist
+    Get local machine total attached gpu devices
 
     Returns:
         Total attached GPU count
 
     """
 
-    return int(command_parser("nvidia-smi --query-gpu=count --format=csv,noheader").pop())
+    return int(command_parser("nvidia-smi --query-gpu=count --format=csv,noheader"))
