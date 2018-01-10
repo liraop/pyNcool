0c8a7b0c344621f8c8c5c798f2c950824935ffda
liraop <lirapdo@gmail.com>
2018-01-09 21:25:06 -0300

    get GPU temperature

--- GPUDataGatherer.py
+++ GPUDataGatherer.py
@@ -2,6 +2,12 @@ from abc import ABC, abstractmethod
 import Utils
 
 class GPUDataFetcher(ABC):
+    """
+    A class/interface/metaclass to gather data from GPU.
+
+    This design choice was made to promote the use both nvidia-smi and nvidia-settings commands
+    as well any other agent to gather data from the actual hardware GPU.
+    """
 
     @abstractmethod
     def get_gpu_count(self):
@@ -17,11 +23,21 @@ class GPUDataFetcher(ABC):
     @abstractmethod
     def get_gpu_info(self, slot):
         """
-        Get data from GPU in `slot`
+        Get identification from GPU in `slot`
 
         Returns:
             {slot,name} dict
 
+        """
+        pass
+
+    @abstractmethod
+    def get_gpu_temperature(self, slot):
+        """
+        Get core temperature from GPU in `slot`
+
+        Returns:
+            Temperature in Celcius
         """
         pass
 
@@ -43,3 +59,10 @@ class SMIFetcher(GPUDataFetcher):
         gpu_data["name"] = Utils.command_parser(command).strip()
 
         return gpu_data
+
+    def get_gpu_temperature(self, slot):
+
+        command = "nvidia-smi -i %d --query-gpu=temperature.gpu --format=csv,noheader" % slot
+        temperature = Utils.command_parser(command).strip()
+
+        return temperature
