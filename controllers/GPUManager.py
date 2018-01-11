c77f7475490822e4fe0d79915967de402ffd9207
liraop <lirapdo@gmail.com>
2018-01-11 13:13:03 -0300

    add update method

--- controllers/GPUManager.py
+++ controllers/GPUManager.py
@@ -7,13 +7,27 @@ A manager lying between data, GPUs and commands.
 
 GPU_SET = []
 
-def add_gpu(GPU):
+def add_gpu(gpu):
     """
-    Add a `GPU` to `slot` in the GPU_SET
+    Add `gpu` in GPU_SET
 
     Args:
-        slot (int): GPU's slot on system.
-        gpu (GPU): GPU object.
+        gpu (GPU): GPU object
 
     """
-    GPU_SET.append(GPU)
+    GPU_SET.append(gpu)
+
+def update(gpu_data_fetcher):
+    """
+    Updates all GPUs temperature and fanspeed using `GPUDataFetcher`
+    Preliminary method. > I better give it a thought later <
+
+    """
+    for gpu in GPU_SET:
+        gpu.temperature = gpu_data_fetcher.get_gpu_temperature(gpu.slot)
+        gpu.fanspeed = gpu_data_fetcher.get_gpu_fanspeed(gpu.slot)
+
+#temporary for testing purproses
+def status():
+    for gpu in GPU_SET:
+        print(gpu)
