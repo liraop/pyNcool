db70b70d094db88b58636127b03da1446f1d076f
liraop <lirapdo@gmail.com>
2018-03-09 23:59:12 -0300

    add temperature control policy module

--- models/__init__.py
+++ models/__init__.py
@@ -5,3 +5,4 @@ GPU models package.
 __all__ = ["gpu"]
 
 from models.gpu import GPU
+from models.temperature_policy import Profile
---
+++ models/temperature_policy.py
@@ -0,0 +1,27 @@
+"""
+Temperature and policies related module
+"""
+
+class Profile(object):
+    """
+    Temperature control profile class
+    """
+
+    def __init__(self, name):
+        """
+        Creates a new temperature Profile
+
+        Args:
+            name (str): profile name
+        """
+
+        self._name = name
+
+    @property
+    def name(self):
+        """Profile name"""
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
